a55f3cdf13c9657895297e356d750039285d8517
Hey <zhxixeng12@gmail.com>
2015-03-12 15:54:01 +0800

    mkvendor.sh: Initial commit of a31twrp

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := a31twrp
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/a31twrp/device_a31twrp.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a31twrp
+PRODUCT_NAME := cm_a31twrp
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := a31twrp
+PRODUCT_MANUFACTURER := htc
